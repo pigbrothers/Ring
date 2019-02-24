a6b70ba8a6e03262e211c48ec17af1db1091d4b5
권혁준 <gurwnsdl1@gwonhyeogjun-ui-MacBookAir.local>
2019-02-24 20:14:42 +0900

    02/22 second Hyuk jun
    테이블뷰에서 검색기능 활성화

--- Ring/FriendViewController.swift
+++ Ring/FriendViewController.swift
@@ -10,41 +10,73 @@ import UIKit
 import Firebase
 import AuthenticationServices
 
-class FriendViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-  
+class FriendViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredTableData.removeAll(keepingCapacity: false)
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (items as NSArray).filtered(using: searchPredicate)
+        filteredTableData = array as! [String]
+        self.tableView.reloadData()
+    }
+    
     
     @IBOutlet weak var tableView: UITableView!
     
     let items: [String] = [(Auth.auth().currentUser?.displayName)!, "swift", "ios"]
-    
+    var resultSearchController = UISearchController()
+    var filteredTableData = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
+        
+        self.resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            controller.searchBar.barStyle = UIBarStyle.black
+            controller.searchBar.barTintColor = UIColor.white
+            controller.searchBar.backgroundColor = UIColor.clear
+            self.tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
+        self.tableView.reloadData()
         // Do any additional setup after loading the view.
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        if self.resultSearchController.isActive {
+            return self.filteredTableData.count
+        }else{
+            return self.items.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MCell")! as UITableViewCell
-        let cell2 = tableView.dequeueReusableCell(withIdentifier: "FCell")! as UITableViewCell
-        cell2.textLabel?.text = items[indexPath.row]
+       // let cell2 = tableView.dequeueReusableCell(withIdentifier: "FCell")! as UITableViewCell
+        //cell2.textLabel?.text = items[indexPath.row]
         cell.textLabel?.text = items[indexPath.row]
         
+        if self.resultSearchController.isActive {
+            cell.textLabel?.text = filteredTableData[indexPath.row]
+        } else {
+            cell.textLabel?.text = items[indexPath.row]
+        }
+        /*  주석을 풀면 cell두개를 내 상황에 따라 자유롭게 움직일수 있다.
         if(items[indexPath.row] == (Auth.auth().currentUser?.displayName)!)
         {
-            
             return cell
         }
         else{
-            
             return cell2
         }
-    
+         */
+        return cell
     }
     
     
