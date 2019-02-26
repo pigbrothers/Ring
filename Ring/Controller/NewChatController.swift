a787f01b553d232635fd137cd33418cf2ef276e8
권혁준 <gurwnsdl1@gwonhyeogjun-ui-MacBookAir.local>
2019-02-26 15:36:29 +0900

    02/26 jy
    주영이의 노력의 결과물

--- Ring/Controller/NewChatController.swift
+++ Ring/Controller/NewChatController.swift
@@ -18,6 +18,7 @@ class NewChatController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancle", style: .plain, target: self, action: #selector(handleCancle))
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
         fetchUser()
     }
     
@@ -26,8 +27,11 @@ class NewChatController: UITableViewController {
             
             if let dictionary = snapshot.value as? [String: AnyObject] {
                 let user = User()
-                user.setValuesForKeys(dictionary)
-                
+
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                self.users.append(user)
+                self.tableView.reloadData()
             }
         }, withCancel: nil)
     }
@@ -43,13 +47,26 @@ class NewChatController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return users.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        cell.textLabel?.text = "Dummy TEXT"
+        //let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
         
         return cell
+    }
+}
+
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
