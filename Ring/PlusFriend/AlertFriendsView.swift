42efa6e474422e31b86eb6ecff51781920cc6bce
권혁준 <gurwnsdl1@gwonhyeogjun-ui-MacBookAir.local>
2019-07-04 01:45:55 +0900

    login and friends
    친구 추가및 로그인 사소한 오류 , 작은 결점 수정

--- Ring/PlusFriend/AlertFriendsView.swift
+++ Ring/PlusFriend/AlertFriendsView.swift
@@ -23,18 +23,46 @@ class AlertFriendsView: UITableViewController {
     }
     
     func fetchUser() {
-        Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
-            
-            if let dictionary = snapshot.value as? [String: AnyObject] {
-                
+        let def =  Database.database().reference().child("users")
+        
+        let nouser = User()
+        var nouserE : [String] = [String]()
+        var booluser : [String] = [String]()
+        
+        def.child(Auth.auth().currentUser!.uid).child("friends").observe(.childAdded, with: { (snapshot) in
+            let dict = snapshot.value as? [String:AnyObject]
+            nouser.email = dict!["email"] as? String
+            nouserE.append(nouser.email!)
+        })
+        
+        
+        def.observe(.childAdded, with: { (snapshot) in
+           if let  dictionary = snapshot.value as? [String: AnyObject]{
+       
                 let user = User()
-                 if user.email != Auth.auth().currentUser?.email {
                 user.name = dictionary["name"] as? String
                 user.email = dictionary["email"] as? String
                 user.profileImageUrl = dictionary["profileImageUrl"] as? String
-                self.users.append(user)
-                self.tableView.reloadData()
-            }
+                let count = nouserE.count
+                     if (dictionary["email"] as? String) != Auth.auth().currentUser?.email {
+                            for i in 0..<count {
+                                if nouserE[i] == user.email{
+                                    booluser.append("true")
+                                }else{
+                                    booluser.append("false")
+                                }
+                            }
+                
+                                if booluser.contains("true") == false {
+                                    self.users.append(user)
+                                    print(user)
+                                    booluser.removeAll()
+                                }
+                                else{
+                                    booluser.removeAll()
+                                }
+                         self.tableView.reloadData()
+                }
             }
         }, withCancel: nil)
     }
